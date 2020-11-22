e01589df21c76d41976fed4d9bdf937070372e3b
justinzm <3907721@qq.com>
2020-11-22 13:20:08 +0800

    2.1

---
+++ app/settings.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+__author__ = 'justin.郑'
+
+"""
+生产环境与开发环境相同的参数
+"""
+
+PER_PAGE = 15
+
+VERSION_ADMIN = 'admin_flask_cms 1.2'
+
+STR_TO_INT = ['status', 'pid', 'sort', 'group_id', 'section_id', 'category_id', 'istop']
+
+DATE_TO_INT = ['start_time']
+
+TOKEN_EXPIRATION = 2 * 3600
+
+# 微信公众号 参数
+
+# TOKEN = 'stock2019'
+# APPID = 'wxda91c5eb944fe49a'
+# APPSECRET = 'aa6950a2fb0aabe21840563fefc06dee'
