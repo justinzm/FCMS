0e982988af872cd8068ffbacdb81576010a73392
justin.zm <3907721@qq.com>
2022-03-13 22:05:55 +0800

    220313

--- app/models/wx_diymenu.py
+++ app/models/wx_diymenu.py
@@ -1,16 +1,15 @@
-#!/usr/bin/python
-# -*- coding:utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/8/22 0022
+# @Author  : justin.郑 3907721@qq.com
+# @File    : wx_diymenu.py
+# @Desc    : 微信公众号-自定义菜单表
 
 from sqlalchemy import Column, Integer, String, Text, Boolean
 from app.models.base import Base
 
-__author__ = 'justin.郑'
-
 
 class WxDiymenu(Base):
-    """
-    微信公众号-自定义菜单表
-    """
     __tablename__ = Base.prefix+'wx_diymenu'
 
     id = Column(Integer, primary_key=True, autoincrement=True, comment="ID")
@@ -18,6 +17,8 @@ class WxDiymenu(Base):
     keyword = Column(String(50), comment="关键词")
     name = Column(String(20), comment="菜单标题")
     type = Column(String(20), comment="类型")
+    appid = Column(String(50), comment="appid")
+    pagepath = Column(String(50), comment="pagepath")
     sort = Column(Integer, comment="排序")
     url = Column(Text, comment="链接地址")
     status = Column(Boolean, default=True, comment="是否发布")
--- app/models/wx_reply.py
+++ app/models/wx_reply.py
@@ -1,16 +1,15 @@
-#!/usr/bin/python
-# -*- coding:utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/8/22 0022
+# @Author  : justin.郑 3907721@qq.com
+# @File    : wx_reply.py
+# @Desc    : 微信公众号-关注回复表
 
 from sqlalchemy import Column, Integer, Text
 from app.models.base import Base
 
-__author__ = 'justin.郑'
-
 
 class WxReply(Base):
-    """
-    微信公众号-关注回复表
-    """
     __tablename__ = Base.prefix+'wx_reply'
 
     id = Column(Integer, primary_key=True, autoincrement=True, comment="ID")
